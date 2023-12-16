4b410b2df67f6caadd770d74f0ebf5082d9fe045
Leigh Klotz <klotz@klotz.me>
2023-12-15 16:02:34 -0800

    update to latest duckduckgo_search==4.1.0 - change to use DDG search - use most recent version, since older ones don't work

--- script.py
+++ script.py
@@ -2,9 +2,7 @@ import gradio as gr
 import modules.shared as shared
 from duckduckgo_search import DDGS
 
-import re
 import urllib
-import html2text
 
 search_access = True
 
@@ -15,36 +13,6 @@ def search_results(query):
             html = html + str(r) + "\n"
     return html
 
-    driver = webdriver.Chrome(service=service,options=options)
-    driver.set_page_load_timeout(3)
-    html = ""
-    output = ""
-    h = html2text.HTML2Text()
-    
-    try:
-        driver.get(url)
-    except TimeoutException:
-        driver.execute_script("window.stop();")
-        try:
-            h.ignore_links = True
-            main_element = driver.find_element(By.CSS_SELECTOR, "[id~='content']")
-            html += h.handle(main_element.get_attribute('innerHTML')) if main_element.text else ""
-        except NoSuchElementException:
-            try:
-                content_element = driver.find_element(By.CSS_SELECTOR, "[class~='content']")
-                html += h.handle(content_element.get_attribute('innerHTML')) if content_element.text else ""
-            except NoSuchElementException:
-                try:
-                   content_element = driver.find_element(By.TAG_NAME, "main")
-                   html += h.handle(content_element.get_attribute('innerHTML')) if content_element.text else ""
-                except NoSuchElementException:
-                    pass
-    except NoSuchElementException as e:
-        pass
-    
-    output = html
-    driver.quit()
-    return output
 
 def ui():
     global search_access
@@ -52,19 +20,18 @@ def ui():
     checkbox.change(fn=update_search_access, inputs=checkbox)
     return checkbox, search_access
 
+
 def update_search_access(checkbox_value):
     global search_access
     search_access = checkbox_value  # assign the value of the checkbox to the variable
     return search_access, checkbox_value
 
+
 def input_modifier(user_input, state):
     global search_access
     if search_access:
-        search_query = re.search(r'search\s+"([^"]+)"', user_input, re.IGNORECASE)
-
-        if search_query:
-            query = search_query.group(1)
-        elif user_input.lower().startswith("search"):
+        if user_input.lower().startswith("search"):
+            shared.processing_message = "*Searching online...*"
             query = user_input.replace("search", "").strip()
             state["context"] = state["context"] + "Relevant search results are in the DuckDuckGo search results. Use this info in the response."
             search_data = search_results(query)
@@ -76,6 +43,7 @@ def input_modifier(user_input, state):
 
 def output_modifier(output):
     return output
+
 
 def bot_prefix_modifier(prefix):
     return prefix
